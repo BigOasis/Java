e5174eedd9221666897dce7de0f32a3a7bbc7e8c
arty0928 <arty0928@konkuk.ac.kr>
2025-08-21 16:02:15 +0900

    ✏️comment: PGS_피로도_주석 추가 및 성능 개선 #2

--- EunseoPark/Week1/PGS_피로도.java
+++ EunseoPark/Week1/PGS_피로도.java
@@ -1,19 +1,26 @@
 import java.util.*;
-
+/*
+  visited를 전역변수 vs 함수 인자로 넘길까 ?   => 함수 인자로 넘기는 것이 더 효율적
+    1. 함수 인자로 넘겨준다고 해서 현재 visited는 배열이므로 배열의 참조값만 넘김, 값을 복사 x (성능 영향 X) 
+    
+    전역변수로 하면 코드가 깔끔하지만, Solutions 함수를 호출할 때마다 잔존 값의 영향 받을 수 있음.
+    함수 인자로 넘기면 배열의 참조값만 넘기므로 성능 영향 X -> 함수 인자로 넘기는 것이 더 빨랐음
+*/
 class Solution {
     
     int result;
-
+    
     public int solution(int k, int[][] dungeons) {
-        int result = 0;
+        result = 0;
         int length = dungeons.length;
         boolean[] visited = new boolean[length];
         
-        return dfs(k, dungeons, length, visited, 0);
+        dfs(k, dungeons, length, visited, 0);
+        return result;
     }
     
-    public int dfs(int power, int[][] dungeons, int length, boolean[] visited, int count){
-        if(result == length) return result;
+    public void dfs(int power, int[][] dungeons, int length, boolean[] visited, int count){
+        if(result == length) return;
         result = Math.max(result, count);
         
         for(int i = 0; i < length; i++){
@@ -24,6 +31,5 @@ class Solution {
                 visited[i] = false;
             }
         }
-        return result;
     }
 }
